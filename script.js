1c498528bd10c5af90cdfa6d5cf761a590027977
Luislev <luis.leiva@utp.ac.pa>
2022-11-27 16:56:17 -0500

    create playRound function and change selection array

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 "strict mode";
 
-const selection = ["Rock", "Paper", "Scissors"];
+const selection = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * selection.length);
   return selection[choice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === "rock") {
+    console.log("You selected rock");
+  } else if (playerSelection.toLowerCase() === "paper") {
+    console.log("You selected paper");
+  } else if (playerSelection.toLowerCase() === "scissors") {
+    console.log("You selected scissors");
+  }
+  console.log(computerSelection);
 }
