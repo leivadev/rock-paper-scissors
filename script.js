a264e9c7cbeb18bcbfb00ddfd02694612f8490e2
Luislev <luis.leiva@utp.ac.pa>
2022-11-27 15:17:21 -0500

    create array of selections and function about computer selection

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "strict mode";
+
+const selection = ["Rock", "Paper", "Scissors"];
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * selection.length);
+  return selection[choice];
+}
