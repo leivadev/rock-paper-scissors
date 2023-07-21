fe960d3adbb47b23cfdcbc52c329d9b52574bc26
Luislev <luis.leiva@utp.ac.pa>
2023-07-21 00:58:10 -0500

    add initial DOM code

--- scriptV2.js
+++ scriptV2.js
@@ -1,1 +1,20 @@
 "strict mode";
+
+const electionElements = document.querySelectorAll('img');
+
+for (let i = 0; i < electionElements.length; i++) {
+    const election = electionElements[i];
+    election.addEventListener('click', function () {
+        switch (i) {
+            case 0:
+                alert('you clicked rock!');
+                break;
+            case 1:
+                alert('you clicked paper!');
+                break;
+            case 2:
+                alert('you clicked scissors!');
+                break;
+        }
+    });
+}
