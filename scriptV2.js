92a5381b3b320077cb195078fc7c14074943187e
Luislev <luis.leiva@utp.ac.pa>
2023-07-30 00:39:13 -0500

    add win and lose functions and reestructure code

--- scriptV2.js
+++ scriptV2.js
@@ -1,59 +1,80 @@
 'use strict';
-
-let playerSelection;
-const playerChoices = document.querySelectorAll('.icons-player img');
+let playerPoints = 0;
+let computerPoints = 0;
 const paragraphText = document.querySelector('.text p');
-const selection = ["rock", "paper", "scissors"];
-const getComputerChoice = () => selection[Math.floor(Math.random() * selection.length)];
+const userScore = document.getElementById('user-points');
+const computerScore = document.getElementById('computer-points');
+const scoreBoard = document.querySelector('.score-board');
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
 
-const playRound = function () {
-    for (let i = 0; i < playerChoices.length; i++) {
-        const choice = playerChoices[i];
-        choice.addEventListener('click', function () {
-            switch (i) {
-                case 0:
-                    playerSelection = 'rock';
-                    break;
-                case 1:
-                    playerSelection = 'paper';
-                    break;
-                case 2:
-                    playerSelection = 'scissors';
-                    break;
-            }
-            let computerSelection = getComputerChoice();
-            if (playerSelection === computerSelection) {
-                paragraphText.textContent = `Draw! Both selected ${computerSelection}.`;
-            } else if (playerSelection === "paper") {
-                switch (computerSelection) {
-                    case "rock":
-                        paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-                        break;
-                    case "scissors":
-                        paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-                        break;
-                }
-            } else if (playerSelection === "scissors") {
-                switch (computerSelection) {
-                    case "rock":
-                        paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-                        break;
-                    case "paper":
-                        paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-                        break;
-                }
-            } else if (playerSelection === "rock") {
-                switch (computerSelection) {
-                    case "paper":
-                        paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-                        break;
-                    case "scissors":
-                        paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-                        break;
-                }
-            }
-        });
+const getComputerChoice = () => {
+    const selection = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * selection.length);
+    return selection[randomNumber];
+}
+
+const win = function () {
+    playerPoints++;
+    userScore.innerHTML = playerPoints;
+    computerScore.innerHTML = computerPoints;
+}
+
+const lose = function () {
+    computerPoints++;
+    userScore.innerHTML = playerPoints;
+    computerScore.innerHTML = computerPoints;
+}
+
+const playRound = function (playerSelection) {
+    if (playerPoints === 5 || computerPoints === 5) {
+        return paragraphText.textContent = "Game Over. Reached 5 rounds.";
+    }
+
+    let computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        return paragraphText.textContent = `Draw! Both selected ${computerSelection}.`;
+    } else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                win();
+                return paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+            case "scissors":
+                lose();
+                return paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "rock":
+                lose();
+                return paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+            case "paper":
+                win();
+                return paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+    } else if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "paper":
+                win();
+                return paragraphText.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+            case "scissors":
+                lose();
+                return paragraphText.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
     }
 }
 
-playRound();
+const game = function () {
+    rock_div.addEventListener('click', function () {
+        playRound('rock');
+    })
+    paper_div.addEventListener('click', function () {
+        playRound('paper');
+    })
+    scissors_div.addEventListener('click', function () {
+        playRound('scissors');
+    })
+}
+
+game();
